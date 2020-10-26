c83afc49aad6b049ea36108551c16a4d11c8b126
Mooks <cmbageant1@gmail.com>
2020-10-25 23:05:58 -0700

    fixed return functions

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var answerEl = document.getElementById("answer");
 var lengthEl = document.getElementById("length");
 var numberEl = document.getElementById("number");
-var lowerEl = document.getElementById("lower");
+var lowerEl = daddocument.getElementById("lower");
 var upperEl = document.getElementById("upper");
 var symbolEl = document.getElementById("symbol");
 var copyEl = document.getElementById("copy");
@@ -20,15 +20,15 @@ function writePassword() {
 }
 
 function getRandomUpperCase(){
-  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+  return String.fromCharCode(Math.floor(Math.random()*26)+1);
 }
 
 function getRandomLowerCase(){
-  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+  return String.fromCharCode(Math.floor(Math.random()*26)+1);
 }
 
 function getRandomNumber(){
-  return String.fromCharCode(math.floor(Math.random()*10)+48);
+  return String.fromCharCode(math.floor(Math.random()*10)+1);
 }
 
 function getRandomSymbol(){
